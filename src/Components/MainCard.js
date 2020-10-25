cdce6cbf7cf258f8c1c7f54d534eefe14a524b63
Myrteza <myrteza@hotmail.fr>
2020-10-25 17:17:35 +0100

    mon premier commit

---
+++ src/Components/MainCard.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import './MainCard.css'; 
+import Header from './Header'; 
+
+
+function MainCard(){
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default MainCard; 
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MainCard from './Components/MainCard';
+
+ReactDOM.render(
+    <MainCard />,
+  document.getElementById('root')
+);
